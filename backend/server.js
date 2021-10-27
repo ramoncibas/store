65f805f5c110d9b59fcaf039d672034c2205eb17
Ramon Cibas <ramon.cibas@hotmail.com>
2021-10-26 22:37:12 -0300

    fix the return of the api values

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const session = require('express-session');
-const fileupload = require('express-fileupload');
-const fs = require('fs');
-const cors = require('cors');
-var path = require('path');
+const express = require("express");
+const session = require("express-session");
+const fileupload = require("express-fileupload");
+const fs = require("fs");
+const cors = require("cors");
+var path = require("path");
 const page = require("./pages");
+const db = require("./database/db");
 
 const app = express();
 
@@ -12,16 +13,27 @@ app.use(cors());
 // app.use(session({secret: 'key'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(fileupload({
-  useTempFiles: true,
-  tempFileDir: path.join(__dirname, 'temp')
-}));
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+  })
+);
 
 // Rotas
-app.get('/', page.getAllProducts);
-app.post('/profile', page.saveUser);
-app.get('/cart', page.getShoppingCartProduct);
-app.post('/add-product', page.saveProduct);
+app.get("/", (req, res) => {
+  db.serialize(() => {
+    db.all(`SELECT * FROM product`, [], (err, rows) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(rows);
+    });
+  });
+});
+app.post("/profile", page.saveUser);
+app.get("/cart", page.getShoppingCartProduct);
+app.post("/add-product", page.saveProduct);
 
 app.listen(5000, () => {
   console.log("Server is running at port 5000...");
