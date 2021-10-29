e976ab2637bd4d818cd1f9fbb667525117122f00
Ramon Cibas <ramon.cibas@hotmail.com>
2021-10-28 21:23:49 -0300

    configure routes

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ const fs = require("fs");
 const cors = require("cors");
 var path = require("path");
 const page = require("./pages");
-const db = require("./database/db");
-
 const app = express();
+const db = require("./database/db");
+const getAllProducts = require("./utils/getAllProducts");
 
 app.use(cors());
 // app.use(session({secret: 'key'}));
@@ -21,6 +21,10 @@ app.use(
 );
 
 // Rotas
+// app.get("/", (req,res) =>  res.send(getAllProducts(db)))
+// app.post("/profile", page.saveUser);
+// app.get("/cart", page.getShoppingCartProduct);
+// app.post("/add-product", page.saveProduct);
 app.get("/", (req, res) => {
   db.serialize(() => {
     db.all(`SELECT * FROM product`, [], (err, rows) => {
@@ -31,10 +35,77 @@ app.get("/", (req, res) => {
     });
   });
 });
-app.post("/profile", page.saveUser);
-app.get("/cart", page.getShoppingCartProduct);
-app.post("/add-product", page.saveProduct);
+
+app.post("/profile", (req, res) => {
+  const { name, email, phone, user_picture } = req.body;
+  console.log(req.body);
+
+  db.serialize(() => {
+    db.run(
+      `INSERT INTO user (name, email, phone, user_picture) VALUES (?, ?, ?, ?);`,
+      [name, email, phone, user_picture],
+      (error) => console.log
+    );
+  });
+});
+
+// Arrumar o get do profile, já esta salvando normalmente
+app.get("/profile/:id", (req, res) => {
+  const id = req.params.id;
+  db.serialize(() => {
+    db.all(`SELECT * FROM user WHERE id = "${id}"`, [], (err, rows) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(rows);
+    });
+  });
+});
+
+// Salva o produto no carrinho de produtos
+app.post("/", (req, res) => {
+  const { id, name, product_picture, price, discount, number_of_installments } = req.body;
+  db.serialize(() => {
+    db.run(
+      `
+      INSERT INTO shopping_cart (product_id, name, product_picture, price, discount, number_of_installments)
+      VALUES (?, ?, ?, ?, ?, ?);`,
+      [id, name, product_picture, price, discount, number_of_installments],
+      (err) => console.log(err)
+    );
+  });
+  return res.redirect("cart");
+});
+
+// Seleciona tudo qu estiver salvo no carrinho
+app.get("/cart", (req, res) => {  
+  db.serialize(() => {
+    db.all(`SELECT * FROM shopping_cart`, [], (err, rows) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(rows);
+    });
+  });
+});
+
+// Adiciona o produto
+app.post("/add-product", (req, res) => {
+  const { name, price, discount, product_picture, number_of_installments } =
+    req.body;
+
+  db.serialize(() => {
+    db.run(
+      `
+      INSERT INTO product (name, price, discount, number_of_installments, product_picture)
+      VALUES (?, ?, ?, ?, ?);`,
+      [name, price, discount, number_of_installments, product_picture],
+      (err) => console.log(err)
+    );
+  });
+  res.redirect("/");
+});
 
 app.listen(5000, () => {
-  console.log("Server is running at port 5000...");
+  console.log("Server is running - Port: 5000...");
 });
