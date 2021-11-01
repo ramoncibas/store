09df0b03622d405af2a9d2bb85b5c97f301c36e8
Ramon Cibas <ramon.cibas@hotmail.com>
2021-11-01 19:25:51 -0300

    refactor the code

--- backend/pages.js
+++ backend/pages.js
@@ -1,14 +1,11 @@
 const Database = require("./database/db");
 const saveUser = require("./utils/saveUsers");
 const saveProduct = require("./utils/saveProduct");
-const getAllProducts = require("./utils/getAllProducts");
+const getAllProductsFromDb = require("./utils/getAllProductsFromDb");
 const getAllShoppingCartProducts = require("./utils/getAllShoppingCartProducts");
+const saveProductOnShoppingCartDb = require("./utils/saveProductOnShoppingCartDb")
 
 module.exports = {
-  index(req, res) {
-    return res.render("index");
-  },
-
   /**
    * Salva um produto e redireciona o usuario para home
    * @param {*} req requisição
@@ -26,16 +23,37 @@ module.exports = {
 
       saveProduct(db, {
         name: fields.name,
-        picture: fields.picture,
         price: fields.price,
         discount: fields.discount,
-        installments: fields.numberOfInstallments,
+        number_of_installments: fields.number_of_installments,
+        product_picture: fields.product_picture,
+      }).then(() => {
+        res.redirect("/");
       });
 
-      return res.redirect("/");
     } catch (error) {
       console.log(error);
       return res.send("Something went wrong, SaveProduct");
+    }
+  },
+
+  saveProductOnShoppingCart(req, res) {
+    const data = req.body;  
+    try {
+      const db = Database;
+
+      saveProductOnShoppingCartDb(db, {
+        id: data.id, 
+        name: data.name, 
+        product_picture: data.product_picture, 
+        price: data.price, 
+        discount: data.discount, 
+        number_of_installments: data.number_of_installments
+      });
+
+    } catch(error) {
+      console.log(error)
+      return res.send("Something went wrong, saveProductOnShoppingCart");
     }
   },
 
@@ -53,15 +71,14 @@ module.exports = {
 
     try {
       const db = Database;
-
       saveUser(db, {
-        name: fields.name,
+        name: fields.name, 
         email: fields.email,
         phone: fields.phone,
-        picture: fields.picture,
+        user_picture: fields.user_picture
+      }).then(() => {
+        res.redirect("profile");
       });
-
-      return res.redirect("/profile");
     } catch (error) {
       console.log(error);
       return res.send("Something went wrong, SaveUser");
@@ -75,14 +92,17 @@ module.exports = {
    * @returns uma coleção de usuarios
    */
   selectUser(req, res) {
+    const id = req.params.id;
     try {
-      const db = Database;
-      const user = db.run("SELECT MAX(id) FROM user");
-      return res.render("/profile", { user });
+      // Code...
     } catch (error) {
       console.log(error);
       return res.send("Something went wrong, Select All Users");
     }
+  },
+
+  getUser(req, res) {
+    // Code...
   },
 
   /**
@@ -94,9 +114,7 @@ module.exports = {
   getAllProducts(req, res) {
     try {
       const db = Database;
-      const products = getAllProducts(db);
-      console.log(products);
-      return res.render("/", { products });
+      getAllProductsFromDb(db).then((products) => res.send(products));
     } catch (error) {
       console.log(error);
       return res.send("Something went wrong, Select All Products");
@@ -113,8 +131,7 @@ module.exports = {
     //const id = req.query.id;
     try {
       const db = Database;
-      const products = getAllShoppingCartProducts(db);
-      return res.render("/cart", { products });
+      getAllShoppingCartProducts(db).then((products) => res.send(products));
     } catch (error) {
       console.log(error);
       return res.send("Something went wrong, Select Shopping Cart");
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 var path = require("path");
 const page = require("./pages");
 const app = express();
-const db = require("./database/db");
-const getAllProducts = require("./utils/getAllProducts");
 
 app.use(cors());
 // app.use(session({secret: 'key'}));
@@ -21,90 +19,12 @@ app.use(
 );
 
 // Rotas
-// app.get("/", (req,res) =>  res.send(getAllProducts(db)))
-// app.post("/profile", page.saveUser);
-// app.get("/cart", page.getShoppingCartProduct);
-// app.post("/add-product", page.saveProduct);
-app.get("/", (req, res) => {
-  db.serialize(() => {
-    db.all(`SELECT * FROM product`, [], (err, rows) => {
-      if (err) {
-        console.log(err);
-      }
-      res.send(rows);
-    });
-  });
-});
-
-app.post("/profile", (req, res) => {
-  const { name, email, phone, user_picture } = req.body;
-  console.log(req.body);
-
-  db.serialize(() => {
-    db.run(
-      `INSERT INTO user (name, email, phone, user_picture) VALUES (?, ?, ?, ?);`,
-      [name, email, phone, user_picture],
-      (error) => console.log
-    );
-  });
-});
-
-// Arrumar o get do profile, já esta salvando normalmente
-app.get("/profile/:id", (req, res) => {
-  const id = req.params.id;
-  db.serialize(() => {
-    db.all(`SELECT * FROM user WHERE id = "${id}"`, [], (err, rows) => {
-      if (err) {
-        console.log(err);
-      }
-      res.send(rows);
-    });
-  });
-});
-
-// Salva o produto no carrinho de produtos
-app.post("/", (req, res) => {
-  const { id, name, product_picture, price, discount, number_of_installments } = req.body;
-  db.serialize(() => {
-    db.run(
-      `
-      INSERT INTO shopping_cart (product_id, name, product_picture, price, discount, number_of_installments)
-      VALUES (?, ?, ?, ?, ?, ?);`,
-      [id, name, product_picture, price, discount, number_of_installments],
-      (err) => console.log(err)
-    );
-  });
-  return res.redirect("cart");
-});
-
-// Seleciona tudo qu estiver salvo no carrinho
-app.get("/cart", (req, res) => {  
-  db.serialize(() => {
-    db.all(`SELECT * FROM shopping_cart`, [], (err, rows) => {
-      if (err) {
-        console.log(err);
-      }
-      res.send(rows);
-    });
-  });
-});
-
-// Adiciona o produto
-app.post("/add-product", (req, res) => {
-  const { name, price, discount, product_picture, number_of_installments } =
-    req.body;
-
-  db.serialize(() => {
-    db.run(
-      `
-      INSERT INTO product (name, price, discount, number_of_installments, product_picture)
-      VALUES (?, ?, ?, ?, ?);`,
-      [name, price, discount, number_of_installments, product_picture],
-      (err) => console.log(err)
-    );
-  });
-  res.redirect("/");
-});
+app.get("/", page.getAllProducts);
+app.post("/", page.saveProductOnShoppingCart);
+app.post("/profile", page.saveUser);
+app.get("/profile/:id", page.getUser);
+app.get("/cart", page.getShoppingCartProduct);
+app.post("/add-product", page.saveProduct);
 
 app.listen(5000, () => {
   console.log("Server is running - Port: 5000...");
