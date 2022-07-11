02cefae3cf50be5c5912334e18bcf2f385b06568
Ramon Cibas <ramon.cibas@hotmail.com>
2022-07-11 20:27:50 -0300

    add component to render if the cart is null

--- src/view/ShoppingCart/index.jsx
+++ src/view/ShoppingCart/index.jsx
@@ -4,6 +4,7 @@ import { Title } from "../../components";
 import { Row, Col } from "react-bootstrap";
 import Card from "./Card";
 import api from "../../utils/api";
+import {ProductNull} from "./style";
 
 const ShoppingCart = () => {
   const [products, setProducts] = useState([]);
@@ -13,20 +14,36 @@ const ShoppingCart = () => {
     api.get("/cart").then(({ data }) => {
       setProducts(data);
     });
+
   }, []);
+
+  const ShoppingCartIsNull = () => (
+    <ProductIsNull>
+      <div className="content">
+        <h3 className="info-text">
+          Você ainda não tem nenhum produto no carrinho 😔
+        </h3>
+        <a href="/">Adicionar ao carrinho</a>
+      </div>
+    </ProductIsNull>
+  )
+
   return (
     <Container>
       <Row>
         <Title>Carrinho de Produtos</Title>
       </Row>
       <Row>
-        {products == "" && "Você ainda nao tem nenhum produto :("}
-        {products !== "" &&
-          products.map((product, index) => (
-            <Col key={index}>
-              <Card {...product} />
-            </Col>
-          ))}
+        {
+          // Valida se o usuário já adicionou algum produto no carrinho
+          products.length
+            ? products.map((product, index) => (
+              <Col key={index}>
+                <Card {...product} />
+              </Col>
+            ))
+            : <ShoppingCartIsNull />
+        }
       </Row>
     </Container>
   );
