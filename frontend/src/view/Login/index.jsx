e744e696ed339af1fa7153e659e9e3b9f01f96f4
ramoncibas <ramon.cibas@hotmail.com>
2022-08-08 23:02:07 -0300

    add login page

---
+++ frontend/src/view/Login/index.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Container } from "./style";
+
+const Login = () => {  
+  
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Login;
