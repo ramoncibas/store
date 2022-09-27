f7077757f2b00c7bf7c35ce2857ad893e27e139c
Ramon Cibas <ramon.cibas@hotmail.com>
2022-09-26 22:30:10 -0300

    change the schema of table product
    create 1 new columns in product table, and reshape 3 columns:
    
    discount_percentage [remodeled]
    product_price [remodeled]
    number_of_installments [remodeled]
    free_shipping [new]
    
    this commit also changes the column data types

--- backend/database/schema.js
+++ backend/database/schema.js
@@ -1,22 +1,25 @@
 const schema = `
 CREATE TABLE IF NOT EXISTS user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  email TEXT,
-  phone TEXT,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  phone TEXT NOT NULL,
   user_picture TEXT
 );
 CREATE TABLE IF NOT EXISTS product (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  price TEXT,
-  discount TEXT,
-  number_of_installments TEXT,
-  product_picture TEXT
+  name TEXT NOT NULL,
+  price TEXT NOT NULL,
+  discount_percentage INTEGER DEFAULT 0 NOT NULL,
+  product_price TEXT NOT NULL,
+  number_of_installments INTEGER DEFAULT 0 NOT NULL,
+  free_shipping BIT DEFAULT 0 NOT NULL
 );  
 CREATE TABLE IF NOT EXISTS shopping_cart (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  product_id TEXT,
+  product_id TEXT NOT NULL,
+  --Não preciso disso, posso referenciar so o id com o product
+  --Fazer essa remoção, quando for implementar a feature de habilitar somente produtos cadastrados no banco, para comprar
   name TEXT,
   product_picture TEXT,
   price TEXT,
