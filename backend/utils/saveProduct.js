69d4e78fa4c857332eae044b817181c674d1603f
Ramon Cibas <ramon.cibas@hotmail.com>
2022-09-26 22:30:59 -0300

    change the structure to save the product

--- backend/utils/saveProduct.js
+++ backend/utils/saveProduct.js
@@ -1,16 +1,24 @@
-const saveProduct = function (db, product) {
+const saveProduct = function (db, product) {  
   return new Promise(function (resolve, reject) {
     db.serialize(() => {
       db.run(
-        `INSERT INTO product (name, price, discount, number_of_installments, product_picture) VALUES (?, ?, ?, ?, ?);`,
-        [
+        `INSERT INTO product (
+            name,
+            price,
+            product_picture,
+            discount_percentage,
+            number_of_installments,
+            free_shipping,
+          ) VALUES (?, ?, ?, ?, ?, ?);`
+        ,[
           product.name,
           product.price,
-          product.discount,
-          product.number_of_installments,
           product.product_picture,
+          product.discount_percentage,
+          product.number_of_installments,
+          product.free_shipping
         ],
-        (error) => console.log
+        (error) => console.log(error)
       );
     });
   });
