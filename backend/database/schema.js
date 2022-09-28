09f129ce0607e000a28f3d89c34980e8b9b41a5e
ramoncibas <ramon.cibas@hotmail.com>
2022-09-27 22:11:09 -0300

    fix the schema

--- backend/database/schema.js
+++ backend/database/schema.js
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS product (
   name TEXT NOT NULL,
   price TEXT NOT NULL,
   discount_percentage INTEGER DEFAULT 0 NOT NULL,
-  product_price TEXT NOT NULL,
+  product_picture TEXT NOT NULL,
   number_of_installments INTEGER DEFAULT 0 NOT NULL,
   free_shipping BIT DEFAULT 0 NOT NULL
 );  
