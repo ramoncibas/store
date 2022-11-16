e51227c6fb1a51667d84323414f747fb69afc702
Ramon Cibas <ramon.cibas@hotmail.com>
2022-11-16 20:55:00 -0300

    fix: sets the declaration of the fereign key and the not null type

--- backend/src/schema/product.js
+++ backend/src/schema/product.js
@@ -9,12 +9,12 @@ const ProductSchema = `
     product_picture TEXT NOT NULL,
     number_of_installments INTEGER DEFAULT 0 NOT NULL,
     free_shipping BIT DEFAULT 0 NOT NULL,
-    brand_product_id INTEGER NULL,
-    gender_product_id INTEGER NULL,
-    category_product_id INTEGER NULL,
-    FOREIGN KEY(brand_id) REFERENCES brand_product(id),
-    FOREIGN KEY(gender_id) REFERENCES gender_product(id),
-    FOREIGN KEY(category_id) REFERENCES category_product(id)
+    brand_product_id INTEGER NOT NULL,
+    gender_product_id INTEGER NOT NULL,
+    category_product_id INTEGER NOT NULL,
+    FOREIGN KEY(brand_product_id) REFERENCES brand_product(id),
+    FOREIGN KEY(gender_product_id) REFERENCES gender_product(id),
+    FOREIGN KEY(category_product_id) REFERENCES category_product(id)
   );
 `;
 
