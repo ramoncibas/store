bceb0b1106040d1e486bdd4b69e2b3e32158552d
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-20 18:42:41 -0300

    fix: disable passing tokens
    * disable passing token in "req.body.token" and "req.query.token"

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers["x-access-token"];
+  const token = req.headers["x-access-token"]; //req.body.token || req.query.token ||   
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
@@ -11,8 +11,9 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
-    
-  } catch (err) {
+
+  } catch (error) {
+    console.log(error)
     return res.status(401).send("Invalid Token");
   }
   return next();
