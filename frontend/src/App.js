95d20cbc9e5ae10f8244b447b8d185cb783630c4
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-20 18:53:49 -0300

    feat: import filter component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
 import { NavBar } from "./components";
-import Routes from "./routes";
+import Router from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Filter from "./view/Home/components/Filter";
 
 function App() {
   return (
+    // <div style={{width: '100%', height: '100%', margin: 'auto', display: 'grid', placeItems: 'center'}}>
+    //   <div style={{width: '300px'}}>
+    //     <Filter />
+    //   </div>
+    // </div>
     <div className="App">
       <NavBar />
-      <Routes />
+      <Router />
     </div>
   );
 }
