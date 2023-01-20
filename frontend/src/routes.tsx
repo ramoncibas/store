10f7c6da2085bb1e68618c3b42aa7d3a14fb9161
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-20 18:45:57 -0300

    feat: add login and register page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,12 +4,16 @@ import Home from "./view/Home";
 import UserProfile from "./view/UserProfile";
 import Product from "./view/Product";
 import ShoppingCart from "./view/ShoppingCart";
+import Login from "./view/Login";
+import Register from "./view/Register";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/cart" element={<ShoppingCart />} />
         <Route path="/product" element={<Product />} />
