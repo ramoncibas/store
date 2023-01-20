bef6774371eac00cfdb8a5bf0e84643c82a35fd6
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-20 18:40:18 -0300

    fix: change data types
    * add column: free_shipping
    * rename column "discount" to "discount_percentage"

--- backend/src/schema/shopping_cart.js
+++ backend/src/schema/shopping_cart.js
@@ -2,11 +2,12 @@ const ShoppingCartSchema = `
   CREATE TABLE IF NOT EXISTS shopping_cart (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_id TEXT NOT NULL,    
-    name TEXT,
-    product_picture TEXT,
-    price TEXT,
-    discount TEXT,
-    number_of_installments TEXT
+    name TEXT NOT NULL,
+    product_picture TEXT NOT NULL,
+    price TEXT NOT NULL,
+    number_of_installments TEXT NOT NULL,
+    discount_percentage INTEGER DEFAULT 0 NOT NULL,
+    free_shipping BIT DEFAULT 0 NOT NULL
   );
 `;
 
