fa73f01ee9b2ab03a58bfdc42a6043c3f20ecfcb
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-04-29 22:21:34 -0300

    feat: add new routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./view/Home";
 import UserProfile from "./view/UserProfile";
-import Product from "./view/Product";
+import ProductPage from "./view/Product/";
 import ShoppingCart from "./view/ShoppingCart";
 import Login from "./view/Login";
 import Register from "./view/Register";
@@ -15,8 +15,10 @@ function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<UserProfile />} />
+        <Route path="/profile/:id" element={<UserProfile />} />
         <Route path="/cart" element={<ShoppingCart />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
   );
