96b136a3a8596697a01c1948364f5daae36c8c83
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-04-29 22:20:50 -0300

    fix: data types of schema

--- backend/src/schema/shopping_cart.js
+++ backend/src/schema/shopping_cart.js
@@ -3,11 +3,20 @@ const ShoppingCartSchema = `
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_id TEXT NOT NULL,    
     name TEXT NOT NULL,
+    size INTEGER NOT NULL,
+    color TEXT NOT NULL,
     product_picture TEXT NOT NULL,
     price TEXT NOT NULL,
-    number_of_installments TEXT NOT NULL,
-    discount_percentage INTEGER DEFAULT 0 NOT NULL,
-    free_shipping BIT DEFAULT 0 NOT NULL
+    number_of_installments INTEGER DEFAULT 0 NOT NULL,
+    discount_percentage INTEGER DEFAULT 0 NOT NULL,    
+    free_shipping BIT DEFAULT 0 NOT NULL,
+    brand_product_id INTEGER NOT NULL,
+    gender_product_id INTEGER NOT NULL,
+    category_product_id INTEGER NOT NULL,
+    FOREIGN KEY(product_id) REFERENCES product(id),
+    FOREIGN KEY(brand_product_id) REFERENCES brand_product(id),
+    FOREIGN KEY(gender_product_id) REFERENCES gender_product(id),
+    FOREIGN KEY(category_product_id) REFERENCES category_product(id)    
   );
 `;
 
