7ca83ea22ce16ec6f79f864a37301212040eb1bc
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-05-24 22:07:30 -0300

    feat: new schema for product and size
    creates the product and size:
    * schema
    * model

--- backend/src/config/schema.js
+++ backend/src/config/schema.js
@@ -3,6 +3,8 @@ const ProductSchema = require('../schema/product');
 const ShoppingCartSchema = require('../schema/shopping_cart');
 const BrandProductSchema = require('../schema/brand_product');
 const GenderProductSchema = require('../schema/gender_product');
+const SizeProductSchema = require('../schema/color_product');
+const ColorProductSchema = require('../schema/size_product');
 const CategoryProductSchema = require('../schema/category_product');
 const PaymentCollectionSchema = require('../schema/payment_collection');
 const ClientTransactionsSchema = require('../schema/client_transactions');
@@ -13,6 +15,8 @@ const schema = `
   ${ShoppingCartSchema}
   ${CategoryProductSchema}
   ${GenderProductSchema}
+  ${SizeProductSchema}
+  ${ColorProductSchema}
   ${BrandProductSchema}  
   ${ClientTransactionsSchema}
   ${PaymentCollectionSchema}
