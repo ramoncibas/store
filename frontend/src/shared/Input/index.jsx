181fee20f94c6b74b82d03b119e23783772c5ea5
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-05-25 20:20:55 -0300

    fix: components paths

---
+++ frontend/src/shared/Input/index.jsx
@@ -0,0 +1,22 @@
+import { Form, FloatingLabel } from "react-bootstrap";
+
+const Input = (props) => (
+  <>
+    <FloatingLabel
+      controlId={props.id}
+      label={props.label}
+      className={props.className || "mb-3"}
+    >
+      <Form.Control
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        required
+      />
+    </FloatingLabel>
+  </>
+);
+
+export default Input;
