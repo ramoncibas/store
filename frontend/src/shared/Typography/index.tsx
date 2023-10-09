4389edf5d90b8652d3f1e98dcb3f1447c2094e89
Ramon Cibas <ramon.cibas@hotmail.com>
2023-10-09 19:08:11 -0300

    feat: add typography component

---
+++ frontend/src/shared/Typography/index.tsx
@@ -0,0 +1,9 @@
+import { TypographyComponent } from './style';
+
+const Typography = ({ children }: any) => (
+  <TypographyComponent className="typography_shared_component">
+    {children}
+  </TypographyComponent>
+)
+
+export default Typography;
